08d4c047eb0ac8978b0fc299f356060075b910bb
pragathilankapalli <plank002@odu.edu>
2017-10-16 15:11:42 -0400

    updated login page

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ require_once 'php_action/db_connect.php';
 session_start();
 
 if(isset($_SESSION['userId'])){
-  header('location: http://localhost/WebProgramming_Slack/dashboard.php');
+  header('location: http://localhost/slack/dashboard.php');
 }
 
 $errors = array();
@@ -30,14 +30,14 @@ if($_POST){
 		$sql = "SELECT * FROM user WHERE username = '$username'";
 		$result = $connect ->query($sql);
 
-		if($result-> num_rows > 0){
+		if($result-> num_rows == 1){
 
 			$password = md5($password);
 
 			$mainSql = "SELECT * FROM user WHERE username = '$username' AND password = '$password'";
 			$mainResult = $connect -> query($mainSql);
 
-			if($mainResult-> num_rows >0){
+			if($mainResult-> num_rows == 1){
 
 				$value = $mainResult -> fetch_assoc();
 				$user_id = $value['user_id'];
@@ -47,7 +47,7 @@ if($_POST){
 
 				$_SESSION['userId'] = $user_id;
 
-				header('location: http://localhost/WebProgramming_Slack/dashboard.php');
+				header('location: http://localhost/slack/dashboard.php');
 			}else{
 
 			$errors[] = "Incorrect username/password combination";
@@ -60,6 +60,9 @@ if($_POST){
 		}
 	}
 }
+
+
+
 
 ?>
 
@@ -75,50 +78,42 @@ if($_POST){
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 	
-
-
-
+<style>
+body {
+    background-image: url("/WebProgramming_Slack/images/img2.jpg");
+    background-size: cover;
+}
+</style>
 
 </head>
 
-<style>
-
-body {
-    position: relative;
-    overflow-x: hidden;
-
-}
-body,
-html { height: 100%;}
-
-</style>
-
 
 <body>
-
+        <div class="top_block">
+   <h1> <img src="/WebProgramming_Slack/images/img4.jpg" class="img-circle" alt="icon" style="float:left;width:400px;height:400px;"><font size="300">Welcome to Slack</font></h1>
+        </div>
 <div class= "container">
 	<div class="row vertical">
 
 		<div class="col-md-5 col-md-offset-4">
 
-			<div class="panel panel-info">
+			<div class="panel panel-default">
 			  <div class="panel-heading">
-			    <h3 class="panel-title">Please Sign In</h3>
+			    <h3 class="panel-title"> Sign In</h3>
 			  </div>
 			  <div class="panel-body">
 
 			  	<form class="form-horizontal" action="<?php echo $_SERVER['PHP_SELF']?>" method="POST" id="loginForm">
 				  <div class="form-group">
 				    <label for="username" class="col-sm-2 control-label">Username</label>
-				    <div class="col-sm-10">
+				    <div class="col-sm-12">
 				      <input type="text" class="form-control" id="username" name="username" placeholder="Username" required>
 				    </div>
 				  </div>
 				  <div class="form-group">
 				    <label for="password" class="col-sm-2 control-label">Password</label>
-				    <div class="col-sm-10">
+				    <div class="col-sm-12">
 				      <input type="password" class="form-control" id="password" name="password" placeholder="Password" required>
 				    </div>
 				  </div>
@@ -133,7 +128,7 @@ html { height: 100%;}
 				  </div>
 				  <div class="form-group">
 				    <div class="col-sm-offset-2 col-sm-10">
-				      <button type="submit" class="btn btn-default">Sign in</button>
+				      <button type="submit" class="btn btn-success">Submit</button>
 				    </div>
 				  </div>
 				</form>
@@ -143,5 +138,9 @@ html { height: 100%;}
 	</div>
 
 </div>
+
+
+
 </body>
+
 </html>
