bb8859b029595c40528a05139487696e4d74d30d
Nandith <31702387+nmala001@users.noreply.github.com>
2017-10-16 03:44:37 -0400

    Updated Logout Page

--- logout.php
+++ logout.php
@@ -3,6 +3,6 @@ session_start();
 unset($_SESSION['username']);
 session_destroy();
 
-header("Location: http://localhost/slack/index.php");
+header("Location: http://localhost/WebProgramming_Slack/index.php");
 exit;
 ?>
