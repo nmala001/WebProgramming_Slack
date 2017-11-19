9b223d2e169a902a5b1bcdbf8c0cf5498f194f5a
Nandith <31702387+nmala001@users.noreply.github.com>
2017-11-19 18:09:18 -0500

    Made changes to service

--- createnewmsgforchannel.php
+++ createnewmsgforchannel.php
@@ -1,7 +1,6 @@
 <?php
 /*to insert messages*/
 session_start();
-<<<<<<< HEAD
 require_once('./php_action/db_connect.php');
 
      $replyid= $_POST["replymsgid"];
@@ -13,16 +12,8 @@ require_once('./php_action/db_connect.php');
 
 
 $sql = "INSERT INTO `message` (message_id,created_by,created_time,reply_msg_id, message,ch_id) VALUES (NULL,".$_SESSION['userId'].", CURRENT_TIMESTAMP,".$replyid.", '".$message."', ".$channel_id.")";
-=======
-require_once './php_action/db_connect.php';
 
-   /*$created_by= $_SESSION['user_id'];*/
-   $channel_id= mysqli_real_escape_string($connect, $_REQUEST['channel_id']);
-   $message = mysqli_real_escape_string($connect, htmlspecialchars($_REQUEST['message']));
-     $reply_msg_id= mysqli_real_escape_string($connect, $_REQUEST['replymsgid']);
 
-$sql = "INSERT INTO `message` (message_id,created_by,created_time,reply_msg_id, message,ch_id) VALUES (NULL,".$_SESSION['userId'].", CURRENT_TIMESTAMP,".$reply_msg_id.",' ".$message."',".$channel_id.")";
->>>>>>> 9c074763af46e20b800c3078d0a4dfdf244c5eee
 
     if($connect->query($sql)===TRUE)
     {
