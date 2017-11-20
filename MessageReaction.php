65ae1fe0ae62987e6cdb215ea61ad5ca92f47d4b
kavyashreesp <31518105+kavyashreesiri@users.noreply.github.com>
2017-11-20 11:26:35 +0530

    Updated Message Reaction service

--- MessageReaction.php
+++ MessageReaction.php
@@ -6,6 +6,7 @@ $messageid=$_REQUEST['message_id'];
 $userid=$_REQUEST['user_id'];
 
 $sql="SELECT reaction FROM reactions WHERE message_id=$messageid AND user_id=$userid";
+//echo $sql;
 $result = $connect->query($sql);
 $row = mysqli_fetch_row($result);
 $reaction=$row[0];
@@ -18,10 +19,17 @@ if($result->num_rows == 0)
 	{
 		/*$reaction=$_REQUEST['reaction'];*/
 		$insert="INSERT INTO reactions (`reaction_id`, `user_id`, `message_id`, `reaction`) VALUES (NULL,$userid,$messageid,$user_reaction)";
-		
+		//echo $insert;
 		if($connect->query($insert)==TRUE) 
 		{
-			echo "Success";
+			//echo "Success";
+			$sql="SELECT count(*)as num,reaction FROM `reactions` where message_id=$messageid GROUP BY reaction"; 
+						$result = $connect->query($sql);
+						$messages=array();
+						while($row=$result->fetch_assoc()){
+						$reactions[]=$row;
+							}
+						echo json_encode($reactions);
 		}
 		/*else
 		{
@@ -38,24 +46,38 @@ else if($result->num_rows> 0)
 		/*update reaction record*/
 				if($user_reaction==$reaction)
 				{
-					echo "Already reacted";
+					//echo "Already reacted";
 				}
 				else if($reaction!=$user_reaction)
 				{
-					$reactupdate="UPDATE `reactions` SET `reaction`=$user_reaction WHERE user_id=userid AND message_id=messageid";
 					
+					
+					$reactupdate="UPDATE `reactions` SET `reaction`=$user_reaction WHERE user_id=$userid AND message_id=$messageid";
+					//echo $reactupdate;
 					if($connect->query($reactupdate)==TRUE)
 					{
-						echo "User reaction updated";
+						//echo "User reaction updated";
+
+
+						$sql="SELECT count(*)as num,reaction FROM `reactions` where message_id=$messageid GROUP BY reaction"; 
+						$result = $connect->query($sql);
+						$messages=array();
+						while($row=$result->fetch_assoc()){
+						$reactions[]=$row;
+							}
+						echo json_encode($reactions);
 					}
 					else
 					{
-						echo "Error :" .$reactupdate. "<br>" .$connect->error;
+						//echo "Error :" .$reactupdate. "<br>" .$connect->error;
 					}
 
 				}
 }
 
 ?>
+
+
+
 
 
