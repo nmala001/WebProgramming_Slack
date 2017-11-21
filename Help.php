76933d17c0a507ea9d5bfb0bd1fc8b796ab57159
kavyashreesp <31518105+kavyashreesiri@users.noreply.github.com>
2017-11-21 01:51:27 -0500

    created help pages

---
+++ Help.php
@@ -0,0 +1,123 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Help Page</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <style>
+    /* Remove the navbar's default margin-bottom and rounded borders */ 
+    .navbar {
+      margin-bottom: 0;
+      border-radius: 0;
+    }
+    
+    /* Add a gray background color and some padding to the footer */
+    footer {
+      background-color: #f2f2f2;
+      padding: 25px;
+    }
+  </style>
+</head>
+<body>
+
+<nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>                        
+      </button>
+    </div>
+    <div class="collapse navbar-collapse" id="myNavbar">
+        <!-- <li class="active"><b>Help</b></li> -->
+      <ul class="nav navbar-nav navbar-right">
+
+        <li><a href="dashboard.php"><span class="glyphicon glyphicon-log-in"></span> Cancel</a></li>
+
+
+      </ul>
+
+  </div>
+</nav>
+    <div class="container">
+  <div class="page-header">
+    <h1 class="text-center">Help Page</h1>   
+    <p class="text-center">You can always ask me !!!</p>
+   
+  </div>
+    </div>
+
+
+  
+<div class="container-fluid bg-3 text-center">    
+  <div class="row">
+    <div class="col-sm-3">
+      <p><b>New User Registration</b></p><a href="RegistrationHelp.php">Registration-Help</a>
+    </div>
+
+
+  </div>
+</div>
+<br>
+
+<div class="container-fluid bg-3 text-center">    
+  <div class="row">
+    <div class="col-sm-3">
+      <p><b>Channel Creation</b></p><a href="ChannelcreationHelp.php">Channel creation-Help</a>
+    </div>
+
+ 
+
+  </div>
+</div>
+<br>
+
+<div class="container-fluid bg-3 text-center">    
+  <div class="row">
+    <div class="col-sm-3">
+      <p><b>Uploading Images from Web</b></p><a href="UploadingWebImagesHelp.php">Uploading Web Images-Help</a>
+    </div>
+
+
+  </div>
+</div>
+<br>
+
+
+<div class="container-fluid bg-3 text-center">    
+  <div class="row">
+    <div class="col-sm-3">
+      <p><b>Uploading Local Images</b></p><a href="UploadLocalImagesHelp.php">Upload Local Images-Help</a>
+    </div>
+
+
+  </div>
+</div>
+<br>
+
+<div class="container-fluid bg-3 text-center">    
+  <div class="row">
+    <div class="col-sm-3">
+      <p><b>Updating Profile Picture</b></p><a href="UpdatingProfilePictureHelp.php">Updating Profile Picture-Help</a>
+    </div>
+
+
+  </div>
+</div>
+<br>
+
+
+
+
+
+
+
+
+
+
+</body>
+</html>
