0d1ee881cfe4b245fdde31dc148f3ca5399afe2e
kavyashreesp <31518105+kavyashreesiri@users.noreply.github.com>
2017-11-21 12:43:39 +0530

    updated profilepicture page

--- ProfilePicture.php
+++ ProfilePicture.php
@@ -2,11 +2,12 @@
 require_once 'php_action/db_connect.php';
 session_start();
 $user_id=$_SESSION["userId"];
+//echo $user_id;
 $folder="uploads/images/";
 $result=0;
-echo $user_id;
+//echo $user_id;
 if ($_SERVER ['REQUEST_METHOD'] == "POST") {
-    //echo "In Post";
+    echo "In Post";
     // code to upload image to server and update or insert userpicpath
     if (isset ( $_FILES ['image'] )) {
         $errors = array ();
@@ -35,37 +36,35 @@ if ($_SERVER ['REQUEST_METHOD'] == "POST") {
             echo  "<script>alert('File size must be exactly 2MB!!".$sql."');</script>";
         }
         
-        if (count ( $errors ) === 0) {
+    if (count ( $errors ) === 0) 
+    {
             $status=move_uploaded_file ( $file_tmp, $targetPath );
-            
+            //echo  "<script>alert(".$status.");</script>";
             //echo "Status of file:".$status;
-            
-            $sql="update profile_pic set img_path='".$fileName."' where user_id=".$_SESSION["userId"];
-            
-            if ($connect->query($sql) === TRUE) 
-            {
-                $result=1;
-                echo "<script type='text/javascript'>window.top.window.completeUpload('" . $result . "','" . $targetPath . "');</script> ";
-            } 
-            elseif($connect->query($sql) === FALSE) 
-            {
-                    $sql="INSERT INTO `profile_pic`(`user_id`, `img_id`, `img_path`) VALUES ($user_id,NULL,$fileName)";
-                    if($connect->query($sql) === TRUE)
-                    {
-                        $result=1;
-                    }
-                    else
-                    {
-                        $result=0;
-                        echo  "<script>alert('Error uploading profile pic!!".$sql."');</script>";
-                    }
-            }
-            
-          }
+           
+        
+               $sql="INSERT INTO profile_pic (user_id,img_path) VALUES(".$user_id.",'".$fileName."') ON DUPLICATE KEY UPDATE img_path='".$fileName."',user_id=".$user_id."";
+                //$sql="update profile_pic set img_path='".$fileName."' where user_id=".$_SESSION["userId"]."";
+                
+                if ($connect->query($sql) === TRUE) 
+                {
+                    $result=1;
+                    echo $result;
+                    //echo  "<script>alert("updated");</script>";
+                    echo "<script type='text/javascript'>window.top.window.completeUpload('" . $result . "','" . $targetPath . "');</script> ";
+                    
+                }
+                
+                        else
+                        {
+                            
+                            echo  "<script>alert('Error uploading profile pic!!".$sql."');</script>";
+                        }
+               // }
+        }
         else {
             print_r ( $errors );
         }
     }
 }
 ?>
-
