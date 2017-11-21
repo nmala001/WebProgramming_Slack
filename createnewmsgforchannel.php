165a0707cfebd4d9ca42ea7d9a2675b2ba1fa9be
pragathilankapalli <31631521+pragathilankapalli@users.noreply.github.com>
2017-11-21 22:00:56 +0530

    updated createnewchannel service

--- createnewmsgforchannel.php
+++ createnewmsgforchannel.php
@@ -12,11 +12,11 @@ require_once('./php_action/db_connect.php');
    $message = mysqli_real_escape_string($connect,htmlspecialchars($_POST["message"],ENT_QUOTES,'UTF-8'));
 
 echo $_POST["message"]."<br>";
-echo htmlspecialchars($_POST["message"]);
+//echo htmlspecialchars($_POST["message"]);
 
 $sql = "INSERT INTO `message` (message_id,created_by,created_time,reply_msg_id, message,ch_id) VALUES (NULL,".$_SESSION['userId'].", CURRENT_TIMESTAMP,".$replyid.", '".$message."', ".$channel_id.")";
 
-echo $sql;
+//echo $sql;
 
     if($connect->query($sql)===TRUE)
     {
