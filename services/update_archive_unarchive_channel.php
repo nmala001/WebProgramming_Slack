d7dd822dd883fe087eaf1f880fc5f5885503155d
pragathilankapalli <31631521+pragathilankapalli@users.noreply.github.com>
2017-12-12 10:27:16 -0500

    updated service

--- services/update_archive_unarchive_channel.php
+++ services/update_archive_unarchive_channel.php
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 <?php
 session_start();
 require_once '../php_action/db_connect.php';
@@ -32,41 +32,4 @@ $arcUnarc=$_REQUEST['archUnarch'];
 
 }
 
-
-=======
-<?php
-session_start();
-require_once '../php_action/db_connect.php';
-
-$user = $_SESSION['userId'];
-//$username = $_SESSION['userName'];
-
-
-//Archive Unarchive 
-if($_SERVER["REQUEST_METHOD"]=="POST"){ 
-$cid=$_REQUEST['cid'];
-$arcUnarc=$_REQUEST['archUnarch'];
-//echo($cid);
-//echo($arcUnarc);
-        if ($arcUnarc == 1){
-                            $sql = "UPDATE slack.channel SET archive = 1 WHERE channel_id =".$cid;
-                            if($connect->query($sql)===TRUE){
-                                    echo "Archived the channel Sucessfully";
-                                }else{
-                                        echo "Error :" .$sql."<br>".$connect->error;
-                                }
-                         }else if ($arcUnarc == 0){
-                            $sql = "UPDATE slack.channel SET archive = 0 WHERE channel_id = ".$cid;
-                            if($connect->query($sql)===TRUE){
-                                    echo "UnArchived the channel Sucessfully";
-                                }else{
-                                        echo "Error :" .$sql."<br>".$connect->error;
-                                }
-            }             
-    
-
-}
-
-
->>>>>>> d9fd6299650ee9c3c1c2671c195137aba301a6c6
 ?>
