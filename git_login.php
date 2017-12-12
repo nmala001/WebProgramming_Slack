9a26c3159351fc095c90b7d1921689374c9919f7
Nandith <31702387+nmala001@users.noreply.github.com>
2017-12-12 12:03:10 -0500

    git redirecting page

---
+++ git_login.php
@@ -0,0 +1,18 @@
+<?php
+
+require "init.php";
+
+//This will redirect user to the github authorization page
+
+goToAuthUrl();
+
+
+
+//if no redirection occurs then following shows up
+
+
+echo "operation failed.";
+
+
+
+?>
