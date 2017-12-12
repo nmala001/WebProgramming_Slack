00904c7e4e5f014718681134acca365328a84475
Nandith <31702387+nmala001@users.noreply.github.com>
2017-12-12 13:34:37 -0500

    pre dashboard changes

--- pre_dashboard.php
+++ pre_dashboard.php
@@ -3,88 +3,57 @@ session_start();
 require_once 'php_action/db_connect.php';
 
 echo "In pre_dash page";
+$client_id = "4121dae809fdd1bbec46";
+$redirect_url = "http://nmala001.cs518.cs.odu.edu/dashboard.php";
+$client_secret ="b8194c65ca1393fcb1d9325ac22a222886316041"; 
 
-        $client_id = "4121dae809fdd1bbec46";
-        $redirect_url = "http://nmala001.cs518.cs.odu.edu/dashboard.php";
-    
-        if($_SERVER['REQUEST_METHOD']=='GET'){
-    
-            if(isset($_GET['code'])){
-    
-                $code = $_GET['code'];
-                $post = http_build_query(array(
-    
-                    'client_id' => $client_id,
-                    'client_secret' => 'b8194c65ca1393fcb1d9325ac22a222886316041',
-                    'code' => $code,
-                    ));
+$url = 'https://github.com/login/oauth/access_token';
+
+$fields = array(
+		'client_id' => urlencode("4121dae809fdd1bbec46"),
+		'client_secret' => urlencode("b8194c65ca1393fcb1d9325ac22a222886316041"),
+		'code' => urlencode($_GET['code'])
+);
+
+foreach($fields as $key=>$value) { $fields_string .= $key.'='.$value.'&'; }
+rtrim($fields_string, '&');
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch,CURLOPT_URL, $url);
+curl_setopt($ch,CURLOPT_POST, count($fields));
+curl_setopt($ch, CURLOPT_HTTPHEADER, array('Accept: application/json'));
+curl_setopt($ch,CURLOPT_POSTFIELDS, $fields_string);
+
+$result = json_decode(curl_exec($ch),TRUE);
+echo "access token is after 1st step:".$result["access_token"]."<br>";
+
+curl_close($ch);
+
+$ch = curl_init();
+curl_setopt($ch,CURLOPT_URL,"https://api.github.com/user?access_token=".$result["access_token"]);
+curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
+curl_setopt($ch,CURLOPT_USERAGENT,'http://developer.github.com/v3/#user-agent-required');
+curl_setopt($ch, CURLOPT_HTTPHEADER, array('Accept: application/json'));
+$output=json_decode(curl_exec($ch),TRUE);
+var_dump($output);
+curl_close($ch);
+
+$username=$output["login"];
+$_SESSION["userName"]=$output["login"];
+$_SESSION["payload"]=$output;
+
+            
+            $sql = "SELECT * FROM user WHERE username = '$username'";
+		     $result = $connect ->query($sql);
+            if($result-> num_rows == 1){
+				$value = $result -> fetch_assoc();
+                $_SESSION['userId'] = $value["user_id"];
+            }else{
+            
+            //insert query to put username ;
             }
-    
-            $access_data = file_get_contents("http://github.com/login/oauth/access_token?" .$post);
-            $exploded1 = explode('access_token=', $access_data);
-            $exploded2 = explode('$scope=user', $exploded[1]);
-            $access_token = $exploded2[0];
-    
-            $opts = ['http' =>[
-    
-                      'method' => 'GET',
-                      'header' => ['User-Agent: PHP']
-    
-            ]
-    
-            ];
-		
-	   echo "Access token: ".$access_token;
-    
-            //fetching user data
-    
-            $url = "https://api.github.com/user?access_token =" .$access_token;
-            $context = stream_context_create($opts);
-            $data = file_get_contents($url, false, $context);
-		
-	    var_dump($data);
-            $user_data = json_decode($data, true);
-	    var_dump($user_data);
-            $username = $user_data['login'];
-    
-            //fetching email data
-    
-//             $url1 = "https://api.github.com/user?access_token =" .$access_token;
-//             $emails = file_get_contents($url1, false, $context);
-// 	    var_dump $emails;
-//             $emails = json_decode($emails, true);
-//     	    var_dump(emails);
-		
-// 	    echo $emails;
-//             $email = $emails[0];
-    
-//             $userPayLoad = [
-    
-//             'username' => $username,
-//             'email' => $email,
-//             'fetched_from' => "github"
-    
-//             ];
-          
-          //$_SESSION['userId'] = $user_id;
-    
-//             $_SESSION['payload'] =$userPayLoad;
-    
-//             $_SESSION['userName'] = $username;
-            
-//             $sql = "SELECT * FROM user WHERE username = '$username'";
-// 		     $result = $connect ->query($sql);
-//             if($result-> num_rows == 1){
-// 				$value = $result -> fetch_assoc();
-//                 $_SESSION['userId'] = $value["user_id"];
-//             }else{
-            
-//             //insert query to put username ;
-//             }
             
 
-//             header("location: dashboard.php?uname=".$username);
-    
-           // return $userPayLoad;
-	}
+// header("location: dashboard.php?uname=".$username);
+
 ?> 
